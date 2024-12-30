b90e49f842d3ec83a37595b38e99caf67f139774
Rob Ranf <rob@emiyaconsulting.com>
2024-12-29 16:07:43 -0800

    adding base files for drama type

---
+++ internal/dal/dal.go
@@ -0,0 +1,13 @@
+/*
+internal/dal/dal.go
+Created 12/29/24
+Rob Ranf
+rob@emiyaconsulting.com
+- A short description of what this file does*
+*/
+
+package dal
+
+func main() {
+
+}
---
+++ internal/model/drama.go
@@ -0,0 +1,13 @@
+/*
+internal/model/drama.go
+Created 12/29/24
+Rob Ranf
+rob@emiyaconsulting.com
+- A short description of what this file does*
+*/
+
+package model
+
+func main() {
+
+}
