7969d5590149c744f856246d24fec4721aa711cb
Rob Ranf <rob@emiyaconsulting.com>
2025-01-05 17:19:21 -0800

    starting drama entity

--- internal/model/drama.go
+++ internal/model/drama.go
@@ -8,6 +8,19 @@ rob@emiyaconsulting.com
 
 package model
 
-func main() {
+import "time"
 
+type Drama struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"`
+	Episodes  int32     `json:"episodes"`
+	Runtime   int32     `json:"runtime"`
+	Genres    []string  `json:"genres"` // TODO: Add slice of genres using Genre entity
+	Studio    string    `json:"studio"`
+	// TODO: Add slice of actors using Person entity
+	Deleted bool  `default:"false" json:"deleted"`
+	Version int32 `json:"version"`
 }
