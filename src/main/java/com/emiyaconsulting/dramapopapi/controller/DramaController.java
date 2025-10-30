b4dde94f3a94b07c6e043896c128754169c6dc48
Rob Ranf <rob@emiyaconsulting.com>
2025-10-30 11:16:39 -0700

    started adding tests and fixed save drama controller

--- src/main/java/com/emiyaconsulting/dramapopapi/controller/DramaController.java
+++ src/main/java/com/emiyaconsulting/dramapopapi/controller/DramaController.java
@@ -4,6 +4,7 @@ import com.emiyaconsulting.dramapopapi.model.Drama;
 import com.emiyaconsulting.dramapopapi.service.DramaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +20,19 @@ public class DramaController {
     
     // insert a single drama into the database
     @PostMapping("/drama")
-    public ResponseEntity<Drama> saveDrama(@RequestBody Drama drama) {
+    public ResponseEntity<Drama> saveDrama(@Validated @RequestBody Drama drama) {
+        drama = new Drama();
+        drama.setTitle(drama.getTitle());
+        drama.setYear(drama.getYear());
+        drama.setDescription(drama.getDescription());
+        drama.setEpisodes(drama.getEpisodes());
+        drama.setIconUrl(drama.getIconUrl());
+        
         Drama savedDrama = dramaService.saveDrama(drama);
         return new ResponseEntity<>(savedDrama, HttpStatus.CREATED);
     }
     
+    // get all dramas
     @GetMapping("/dramas")
     public Iterable<Drama> getDramas() {
         return dramaService.getDramas();
