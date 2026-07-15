cee4333a93fc06c6e99a1ad05e5697f605ca6856
Rob Ranf <rob@emiyaconsulting.com>
2026-07-14 17:45:24 -0700

    confirming time zone fixes and commenting out tz sanity check

--- src/main/java/com/emiyaconsulting/dramapopapi/DramapopApiApplication.java
+++ src/main/java/com/emiyaconsulting/dramapopapi/DramapopApiApplication.java
@@ -1,6 +1,5 @@
 package com.emiyaconsulting.dramapopapi;
 
-import com.emiyaconsulting.dramapopapi.utilities.TimeZoneSanityCheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
