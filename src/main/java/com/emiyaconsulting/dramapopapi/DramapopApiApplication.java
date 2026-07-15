89d6d6ef75fef9ced92c75eaeed065ec8183cb2d
Rob Ranf <rob@emiyaconsulting.com>
2026-07-14 17:56:12 -0700

    switching spring config to application.yml

--- src/main/java/com/emiyaconsulting/dramapopapi/DramapopApiApplication.java
+++ src/main/java/com/emiyaconsulting/dramapopapi/DramapopApiApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class DramapopApiApplication {
--- src/main/java/com/emiyaconsulting/dramapopapi/model/User.java
+++ src/main/java/com/emiyaconsulting/dramapopapi/model/User.java
@@ -6,7 +6,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
