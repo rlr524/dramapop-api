6e4e4d1a74f12ee0ee40ef07dd6046936d2ffaaa
Rob Ranf <rob@emiyaconsulting.com>
2026-07-28 17:53:15 -0700

    fixing tostring annotation in dramapersonrole

--- src/main/java/com/emiyaconsulting/dramapopapi/model/DramaPersonRole.java
+++ src/main/java/com/emiyaconsulting/dramapopapi/model/DramaPersonRole.java
@@ -4,28 +4,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "drama_person_role")
-@ToString
 @Getter @Setter @NoArgsConstructor
 public class DramaPersonRole {
     @EmbeddedId
     private DramaPersonRoleId id; // Composite primary key
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("dramaId") // Maps the dramaId from the embedded ID
     @JoinColumn(name = "drama_id")
     private Drama drama;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("personId") // Maps the personId from the embedded ID
     @JoinColumn(name = "person_id")
     private Person person;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("roleId") // Maps the roleId from the embedded ID
     @JoinColumn(name = "role_id")
